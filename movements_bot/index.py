c2b42bb1ddaef824db91aeaf54829bd3d293408c
rusfai <70482477+rusfai@users.noreply.github.com>
2025-05-01 15:28:44 +0400

    Delete movements_bot directory

--- movements_bot/index.py
+++
@@ -1,9 +0,0 @@
-
-TOKEN = ''
-PATH = ''
-
-car_1c_url = ''
-user_1c = ''
-password_1c = ''
-
-copy_group = ''
--- movements_bot/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.filters import StateFilter
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
-
-class movent_states(StatesGroup):
-    year = State()
-    department = State()
-    movent_price = State()
-    wait_photo = State()
